26598e6725d22ccff9bf3d4cde7b08f3820a3c32
wongkenchiang97 <kenken.wkc@gmail.com>
2024-07-07 20:21:34 +0800

    fix task manager bug

--- include/async_algorithm_pipeline/task_manager.h
+++ include/async_algorithm_pipeline/task_manager.h
@@ -3,7 +3,9 @@
 
 #include <atomic>
 #include <condition_variable>
+#include <functional>
 #include <future>
+#include <iostream>
 #include <memory>
 #include <mutex>
 #include <queue>
@@ -15,16 +17,21 @@ namespace task_manager {
 template <typename TaskIn, typename TaskOut>
 class Worker;
 
-template <typename TaskIn, typename TaskOut>
+template <typename TaskIn, typename TaskOut, typename WorkerT>
 class Work {
 public:
-    using Ptr = std::shared_ptr<Work<TaskIn, TaskOut>>;
+    using Ptr = std::shared_ptr<Work<TaskIn, TaskOut, WorkerT>>;
     using Output = std::shared_future<TaskOut>;
     using Task = std::packaged_task<TaskOut()>;
+    using Worker = std::shared_ptr<WorkerT>;
 
-    Output output;
+    Work(const TaskIn _task_in, Worker _worker);
+    virtual ~Work();
+    static typename Work<TaskIn, TaskOut, WorkerT>::Ptr create(const TaskIn _task_in, Worker _worker);
+
+    Worker worker;
     Task task;
-    typename Worker<TaskIn, TaskOut>::Ptr worker;
+    Output output;
 };
 
 template <typename TaskIn, typename TaskOut>
@@ -34,32 +41,39 @@ public:
 
     Worker(/* args */);
     virtual ~Worker();
-    virtual typename Worker<TaskIn, TaskOut>::Ptr clone() const = 0;
-    virtual void setInput(const TaskIn) = 0;
-    virtual typename Work<TaskIn, TaskOut>::Task assignTask(TaskIn) = 0;
+    virtual TaskOut work(TaskIn) = 0;
+
+private:
+    virtual Worker<TaskIn, TaskOut>* clone_impl() const = 0;
 };
 
-template <typename TaskIn, typename TaskOut>
+template <typename TaskIn, typename TaskOut, typename WorkerT>
 class Manager {
 public:
-    Manager(size_t num_workers = std::thread::hardware_concurrency());
+    using Ptr = std::shared_ptr<Manager<TaskIn, TaskOut, WorkerT>>;
+    using Worker = std::shared_ptr<WorkerT>;
+    Manager(Worker worker_prototype, size_t num_workers = std::thread::hardware_concurrency());
     virtual ~Manager();
-    void enqueue(typename Work<TaskIn, TaskOut>::Ptr _work);
+    static typename Manager<TaskIn, TaskOut, WorkerT>::Ptr create(Worker worker_prototype, size_t num_workers = std::thread::hardware_concurrency());
+    void enqueue(TaskIn _task_in);
     void setCallback(std::function<void(TaskOut)> _callback_f);
 
 protected:
     std::atomic_bool stop;
 
 private:
-    void AssignWork();
+    void manage();
 
     std::mutex mt_;
     std::condition_variable cv_;
     std::vector<std::thread> threads_;
-    std::queue<typename Work<TaskIn, TaskOut>::Ptr> works_;
+    Worker worker_prototype_;
+    std::queue<typename Work<TaskIn, TaskOut, WorkerT>::Ptr> works_;
     std::function<void(TaskOut)> callback_;
 };
 
 } // namespace task_manager
+
+#include <async_algorithm_pipeline/task_manager_impl.h>
 
 #endif
---
+++ include/async_algorithm_pipeline/task_manager_impl.h
@@ -0,0 +1,123 @@
+#ifndef TASK_MANAGER_IMPL_H
+#define TASK_MANAGER_IMPL_H
+
+#include <async_algorithm_pipeline/task_manager.h>
+
+namespace task_manager {
+
+template <typename TaskIn, typename TaskOut, typename WorkerT>
+Work<TaskIn, TaskOut, WorkerT>::Work(const TaskIn _task_in, Worker _worker)
+    : worker(_worker)
+    , task(std::bind(&WorkerT::work, worker, _task_in))
+    , output(task.get_future().share())
+{
+}
+
+template <typename TaskIn, typename TaskOut, typename WorkerT>
+Work<TaskIn, TaskOut, WorkerT>::~Work()
+{
+}
+
+template <typename TaskIn, typename TaskOut, typename WorkerT>
+typename Work<TaskIn, TaskOut, WorkerT>::Ptr Work<TaskIn, TaskOut, WorkerT>::create(const TaskIn _task_in, Worker _worker)
+{
+    typename Work<TaskIn, TaskOut, WorkerT>::Ptr obj_ptr;
+    obj_ptr = std::make_shared<Work<TaskIn, TaskOut, WorkerT>>(_task_in, _worker);
+    return obj_ptr;
+}
+
+template <typename TaskIn, typename TaskOut>
+Worker<TaskIn, TaskOut>::Worker(/* args */)
+{
+}
+
+template <typename TaskIn, typename TaskOut>
+Worker<TaskIn, TaskOut>::~Worker()
+{
+}
+
+template <typename TaskIn, typename TaskOut, typename WorkerT>
+Manager<TaskIn, TaskOut, WorkerT>::Manager(Worker worker_prototype, size_t num_workers)
+    : worker_prototype_(worker_prototype)
+    , stop(false)
+{
+    std::cout << "worker_prototype_[&]: " << worker_prototype_ << std::endl;
+    for (size_t i = 0; i < num_workers; i++) {
+        threads_.emplace_back(std::thread(&Manager<TaskIn, TaskOut, WorkerT>::manage, this));
+    }
+}
+
+template <typename TaskIn, typename TaskOut, typename WorkerT>
+Manager<TaskIn, TaskOut, WorkerT>::~Manager()
+{
+    {
+        std::unique_lock<std::mutex> lock(mt_);
+        stop = true;
+    }
+    cv_.notify_all();
+
+    for (auto& thread : threads_) {
+        if (thread.joinable())
+            thread.join();
+    }
+}
+
+template <typename TaskIn, typename TaskOut, typename WorkerT>
+typename Manager<TaskIn, TaskOut, WorkerT>::Ptr Manager<TaskIn, TaskOut, WorkerT>::create(Worker worker_prototype, size_t num_workers)
+{
+    typename Manager<TaskIn, TaskOut, WorkerT>::Ptr obj_ptr;
+    obj_ptr = std::make_shared<Manager<TaskIn, TaskOut, WorkerT>>(worker_prototype, num_workers);
+    return obj_ptr;
+}
+
+template <typename TaskIn, typename TaskOut, typename WorkerT>
+void Manager<TaskIn, TaskOut, WorkerT>::setCallback(std::function<void(TaskOut)> _callback_f)
+{
+    std::unique_lock<std::mutex> lock(mt_);
+    callback_ = _callback_f;
+}
+
+template <typename TaskIn, typename TaskOut, typename WorkerT>
+void Manager<TaskIn, TaskOut, WorkerT>::enqueue(TaskIn _task_in)
+{
+    std::unique_lock<std::mutex> lock(mt_);
+    auto work = Work<TaskIn, TaskOut, WorkerT>::create(_task_in, worker_prototype_->clone());
+    works_.emplace(std::move(work));
+    std::cout << "works_[size]: " << works_.size() << std::endl;
+    cv_.notify_one();
+}
+
+template <typename TaskIn, typename TaskOut, typename WorkerT>
+void Manager<TaskIn, TaskOut, WorkerT>::manage()
+{
+    while (true) {
+        /*Get Task*/
+        typename Work<TaskIn, TaskOut, WorkerT>::Ptr work;
+        {
+            std::unique_lock<std::mutex> lock(mt_);
+            cv_.wait(lock, [this]() {
+                bool wait_predication = stop || !works_.empty();
+                return wait_predication;
+            });
+
+            if (stop)
+                break;
+
+            work = std::move(works_.front());
+            works_.pop();
+        }
+
+        /*Execute Task*/
+        std::cout << "work->task()." << std::endl;
+        work->task();
+        if (callback_ == nullptr)
+            continue;
+
+        /*Execute Callback*/
+        callback_(work->output.get());
+    }
+}
+
+} // namespace task_manager
+
+#endif
